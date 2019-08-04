b7b612bb3ab32fc88f549db3bdfe525ac2583ac2
FISK <ilyinskyy.kv@gmail.com>
2019-08-04 17:35:34 +0300

    Final v1.0.0

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -1,93 +1,155 @@
 import React, { useReducer } from 'react';
-import uuid from 'uuid';
+import axios from 'axios';
 import ContactContext from './contactContext';
 import contactReducer from './contactReducer';
 import {
+  GET_CONTACTS,
   ADD_CONTACT,
   DELETE_CONTACT,
   SET_CURRENT,
   CLEAR_CURRENT,
   UPDATE_CONTACT,
   FILTER_CONTACTS,
-  CLEAR_FILTER
+  CLEAR_CONTACTS,
+  CLEAR_FILTER,
+  CONTACT_ERROR
 } from '../types';
 
 const ContactState = props => {
-  //INIT State
   const initialState = {
-    contacts: [
-      {
-        id: 1,
-        name: 'Walter White',
-        email: 'wall@mail.com',
-        phone: '756-558-999',
-        type: 'personal'
-      },
-      {
-        id: 2,
-        name: 'Kurt waller',
-        email: 'kurt@mail.com',
-        phone: '777-888-999',
-        type: 'professional'
-      },
-      {
-        id: 3,
-        name: 'Jane Doe',
-        email: 'jane@mail.com',
-        phone: '777-888-999',
-        type: 'professional'
-      }
-    ],
+    contacts: null,
     current: null,
-    filtered: null
+    filtered: null,
+    error: null
   };
+
   const [state, dispatch] = useReducer(contactReducer, initialState);
 
-  //!!Methods
-  //Add contact
-  const addContact = contact => {
-    contact.id = uuid.v4();
-    dispatch({ type: ADD_CONTACT, payload: contact });
+  // Get Contacts
+  const getContacts = async () => {
+    try {
+      const res = await axios.get('/api/contacts');
+
+      dispatch({
+        type: GET_CONTACTS,
+        payload: res.data
+      });
+    } catch (err) {
+      dispatch({
+        type: CONTACT_ERROR,
+        payload: err.response.msg
+      });
+    }
   };
-  //Delete Contact
-  const deleteContact = id => {
-    dispatch({ type: DELETE_CONTACT, payload: id });
+
+  // Add Contact
+  const addContact = async contact => {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    };
+
+    try {
+      const res = await axios.post('/api/contacts', contact, config);
+
+      dispatch({
+        type: ADD_CONTACT,
+        payload: res.data
+      });
+    } catch (err) {
+      dispatch({
+        type: CONTACT_ERROR,
+        payload: err.response.msg
+      });
+    }
   };
-  //Set Curent Contact
+
+  // Delete Contact
+  const deleteContact = async id => {
+    try {
+      await axios.delete(`/api/contacts/${id}`);
+
+      dispatch({
+        type: DELETE_CONTACT,
+        payload: id
+      });
+    } catch (err) {
+      dispatch({
+        type: CONTACT_ERROR,
+        payload: err.response.msg
+      });
+    }
+  };
+
+  // Update Contact
+  const updateContact = async contact => {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    };
+
+    try {
+      const res = await axios.put(
+        `/api/contacts/${contact._id}`,
+        contact,
+        config
+      );
+
+      dispatch({
+        type: UPDATE_CONTACT,
+        payload: res.data
+      });
+    } catch (err) {
+      dispatch({
+        type: CONTACT_ERROR,
+        payload: err.response.msg
+      });
+    }
+  };
+
+  // Clear Contacts
+  const clearContacts = () => {
+    dispatch({ type: CLEAR_CONTACTS });
+  };
+
+  // Set Current Contact
   const setCurrent = contact => {
     dispatch({ type: SET_CURRENT, payload: contact });
   };
-  //Clear Current Contact
+
+  // Clear Current Contact
   const clearCurrent = () => {
     dispatch({ type: CLEAR_CURRENT });
   };
-  //Update Contact
-  const updateContact = contact => {
-    dispatch({ type: UPDATE_CONTACT, payload: contact });
-  };
-  //Filter Contact
+
+  // Filter Contacts
   const filterContacts = text => {
     dispatch({ type: FILTER_CONTACTS, payload: text });
   };
-  //Clear Filter
+
+  // Clear Filter
   const clearFilter = () => {
     dispatch({ type: CLEAR_FILTER });
   };
 
-  //PROVIDER OUTPUT
   return (
     <ContactContext.Provider
       value={{
         contacts: state.contacts,
         current: state.current,
         filtered: state.filtered,
+        error: state.error,
         addContact,
         deleteContact,
         setCurrent,
         clearCurrent,
         updateContact,
         filterContacts,
-        clearFilter
+        clearFilter,
+        getContacts,
+        clearContacts
       }}
     >
       {props.children}
--- routes/users.js
+++ routes/users.js
@@ -7,41 +7,46 @@ const { check, validationResult } = require('express-validator');
 
 const User = require('../models/User');
 
-// @route   POST api/user
-// @desc    Register a user
-// @access  Public
+// @route     POST api/users
+// @desc      Regiter a user
+// @access    Public
 router.post(
   '/',
   [
-    check('name', 'Please add Name')
+    check('name', 'Please add name')
       .not()
       .isEmpty(),
-    check('email', 'Please include valid email').isEmail(),
-    check('password', 'Please eter a password with 6 or more char!').isLength({
-      min: 6
-    })
+    check('email', 'Please include a valid email').isEmail(),
+    check(
+      'password',
+      'Please enter a password with 6 or more characters'
+    ).isLength({ min: 6 })
   ],
   async (req, res) => {
-    // res.send(req.body);
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res.status(400).json({ erros: errors.array() });
+      return res.status(400).json({ errors: errors.array() });
     }
 
     const { name, email, password } = req.body;
 
     try {
       let user = await User.findOne({ email });
+
       if (user) {
-        return res.status(400).json({ msg: 'User already exist' });
+        return res.status(400).json({ msg: 'User already exists' });
       }
+
       user = new User({
         name,
         email,
         password
       });
+
       const salt = await bcrypt.genSalt(10);
+
       user.password = await bcrypt.hash(password, salt);
+
       await user.save();
 
       const payload = {
@@ -49,6 +54,7 @@ router.post(
           id: user.id
         }
       };
+
       jwt.sign(
         payload,
         config.get('jwtSecret'),
@@ -61,7 +67,7 @@ router.post(
         }
       );
     } catch (err) {
-      console.error(err.massage);
+      console.error(err.message);
       res.status(500).send('Server Error');
     }
   }
